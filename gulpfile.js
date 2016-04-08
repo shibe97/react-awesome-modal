86a3df6d04e6abd2e1943d2934327593aedff861
kazshiba <kazshiba@yahoo-corp.jp>
2016-04-08 09:40:35 +0900

    [update] webpack-stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var webpack = require('gulp-webpack');
+var webpack = require('webpack-stream');
 var webpackConfig = require('./webpack.config.js');
 
 gulp.task('watch', function(){
