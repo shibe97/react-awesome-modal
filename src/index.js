92a1a4c686697dbf11b3d82bcbda4d5b138e8ade
Henry Moews <henrymoews@posteo.de>
2016-11-05 18:56:08 +0000

    added possibility to calculate position via Javascript in case the webpage is broken
    Current case: in iOS in combination with cordova width and height are interchanged and break design where css position is 'fixed'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import style from './style.js';
 
-export default class Modal extends Component {
+class Modal extends Component {
     constructor(props) {
         super(props);
+
+        const backgroundStyle = {};
+
         let effect = props.effect || 'fadeInDown';
-        this.setSize(effect);
+        this.setSize(props, effect, backgroundStyle);
         this.state = {
             visible : props.visible,
-            style : style[effect]
+            style : style[effect],
+            backgroundStyle,
         }
     }
 
@@ -19,20 +23,36 @@ export default class Modal extends Component {
         this.setSize(nextProps.effect);
     }
 
-    setSize(effect) {
-        if(this.props && this.props.width) {
-            style[effect].panel.width = this.props.width + 'px';
-            style[effect].panel.marginLeft = '-' + this.props.width / 2 + 'px';
-        }
-        if(this.props && this.props.height) {
-            style[effect].panel.height = this.props.height + 'px';
-            style[effect].panel.marginTop = '-' + this.props.height / 2 + 'px';
+    setSize(props, effect, backgroundStyle) {
+        if(props) {
+            if (props.width) {
+                style[effect].panel.width = props.width + 'px';
+                style[effect].panel.marginLeft = '-' + props.width / 2 + 'px';
+            }
+            if (props.height) {
+                style[effect].panel.height = props.height + 'px';
+                style[effect].panel.marginTop = '-' + props.height / 2 + 'px';
+            }
+            if(props.screenWidth){
+                backgroundStyle.width = props.screenWidth
+            }
+            if(props.screenHeight){
+                backgroundStyle.width = props.screenWidth
+            }
+            if(props.calculatePosition){
+                if(props.screenWidth){
+                    style[effect].panel.width = props.screenWidth / 2;
+                }
+                if(props.screenHeight){
+                    style[effect].panel.height = props.screenHeight / 2;
+                }
+            }
         }
     }
 
     render() {
         return (
-            <div>
+            <div style={this.state.backgroundStyle}>
                 <div onClick={this.props.onClickAway ? this.props.onClickAway : false} style={this.state.visible ? this.state.style.mask : this.state.style.maskHidden} />
                 <div style={this.state.visible ? this.state.style.panel : this.state.style.panelHidden}>
                     {this.props.children}
@@ -41,3 +61,19 @@ export default class Modal extends Component {
         );
     }
 }
+
+Modal.propTypes = {
+    effect: React.PropTypes.string,
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    screenWidth: React.PropTypes.number,
+    screenHeight: React.PropTypes.number,
+    calculatePosition: React.PropTypes.bool,
+};
+
+Modal.defaultProps = {
+    effect: "fadeDown",
+    calculatePosition: false,
+};
+
+export default Modal;
