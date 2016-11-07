e83d7e4ce1f7de5890236e92be2a1d8ebe023780
Henry Moews <henrymoews@posteo.de>
2016-11-07 10:21:17 +0000

    Revert "added fadeInAtStaticPosition"
    This reverts commit 27ac066bfbc1e61cca8c3bc8526ace4ecbc5e27a.

--- src/style.js
+++ src/style.js
@@ -2,12 +2,10 @@ import fadeInDown from './effect/fadeInDown';
 import fadeInUp from './effect/fadeInUp';
 import fadeInLeft from './effect/fadeInLeft';
 import fadeInRight from './effect/fadeInRight';
-import fadeAtPosition from './effect/fadeAtPosition';
 
 export default {
     fadeInDown : fadeInDown,
     fadeInUp : fadeInUp,
     fadeInLeft : fadeInLeft,
-    fadeInRight : fadeInRight,
-    fadeAtPosition : fadeAtPosition
+    fadeInRight : fadeInRight
 };
