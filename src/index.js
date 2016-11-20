7fc1391c3323f48b46d664a0319bf28ca8dfa2ce
Diogo Barroso <icylabs.dev@gmail.com>
2016-11-20 13:23:01 +0100

    changed width/height calculation to the setSize() function.

--- src/index.js
+++ src/index.js
@@ -24,39 +24,44 @@ export default class Modal extends Component {
       if(this.props && this.props.styles){
         Object.keys(this.props.styles).forEach((key)=>{
           style[effect].panel[key] = this.props.styles[key];
-          if(key === 'width'){
-            if(this.props.styles[key].charAt(this.props.styles[key].length-1) === '%'){
-              const width = this.props.styles[key].slice(0,-1);
-              style[effect].panel.marginLeft = '-' + width / 2 + '%';
-            }else if(this.props.styles[key].charAt(this.props.styles[key].length-1) === 'x'){
-              const width = this.props.styles[key].slice(0,-2);
-              style[effect].panel.marginLeft = '-' + width / 2 + 'px';
-            }else{
-              style[effect].panel.marginLeft = '-' + width / 2 + 'px';
-            }
-          }else if(key === 'height'){
-            if(this.props.styles[key].charAt(this.props.styles[key].length-1) === '%'){
-              const height = this.props.styles[key].slice(0,-1);
-              style[effect].panel.marginTop = '-' + height / 2 + '%';
-            }else if(this.props.styles[key].charAt(this.props.styles[key].length-1) === 'x'){
-              const height = this.props.styles[key].slice(0,-2);
-              style[effect].panel.marginTop = '-' + height / 2 + 'px';
-            }else{
-              style[effect].panel.marginTop = '-' + height / 2 + 'px';
-            }
-          }
         });
       }
     }
 
     setSize(effect) {
         if(this.props && this.props.width) {
+          if(this.props.width.charAt(this.props.width.length-1) === '%'){
+            //Use Percentage
+            const width = this.props.width.slice(0,-1);
+            style[effect].panel.width = width + '%';
+            style[effect].panel.marginLeft = '-' + width / 2 + '%';
+          }else if(this.props.width.charAt(this.props.width.length-1) === 'x'){
+            //Use Pixels
+            const width = this.props.width.slice(0,-2);
+            style[effect].panel.width = width + 'px';
+            style[effect].panel.marginLeft = '-' + width / 2 + 'px';
+          }else{
+            //Default
             style[effect].panel.width = this.props.width + 'px';
             style[effect].panel.marginLeft = '-' + this.props.width / 2 + 'px';
+          }
         }
         if(this.props && this.props.height) {
+          if(this.props.height.charAt(this.props.height.length-1) === '%'){
+            //Use Percentage
+            const height = this.props.height.slice(0,-1);
+            style[effect].panel.height = height + '%';
+            style[effect].panel.marginTop = '-' + height / 2 + '%';
+          }else if(this.props.height.charAt(this.props.height.length-1) === 'x'){
+            //Use Pixels
+            const height = this.props.height.slice(0,-2);
+            style[effect].panel.height = height + 'px';
+            style[effect].panel.marginTop = '-' + height / 2 + 'px';
+          }else{
+            //Default
             style[effect].panel.height = this.props.height + 'px';
             style[effect].panel.marginTop = '-' + this.props.height / 2 + 'px';
+          }
         }
     }
 
