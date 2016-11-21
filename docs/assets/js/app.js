3aa47fd9fd603e2865129c8e72f9fd082ebc6e60
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-21 12:16:29 +0900

    [add] docs

---
+++ docs/assets/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './demo.js';
+
+ReactDOM.render(
+    <Demo />,
+    document.getElementById('App')
+);
---
+++ docs/assets/js/demo.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Modal from '../../../src/index.js';
+
+export default class Demo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fadeInDown : false,
+            fadeInUp : false,
+            fadeInLeft : false,
+            fadeInRight : false,
+        }
+    }
+
+    open(effect) {
+      this.setState({
+        [effect] : true
+      });
+    }
+
+    close(effect) {
+      this.setState({
+        [effect] : false
+      });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><a href="javascript:void(0);" onClick={() => this.open('fadeInDown')}>fadeInDown</a></li>
+                    <li><a href="javascript:void(0);" onClick={() => this.open('fadeInUp')}>fadeInUp</a></li>
+                    <li><a href="javascript:void(0);" onClick={() => this.open('fadeInLeft')}>fadeInLeft</a></li>
+                    <li><a href="javascript:void(0);" onClick={() => this.open('fadeInRight')}>fadeInRight</a></li>
+                </ul>
+                <Modal visible={this.state.fadeInDown} width="400" height="300" effect="fadeInDown" onClickAway={() => this.close('fadeInDown')}>
+                    <div className="Modal">
+                        <h1 className="Modal__title">FadeInDown</h1>
+                        <a href="javascript:void(0);" onClick={() => this.close('fadeInDown')}>Close</a>
+                    </div>
+                </Modal>
+                <Modal visible={this.state.fadeInUp} width="500" height="300" effect="fadeInUp" onClickAway={() => this.close('fadeInUp')}>
+                    <div className="Modal">
+                        <h1 className="Modal__title">FadeInUp</h1>
+                        <a href="javascript:void(0);" onClick={() => this.close('fadeInUp')}>Close</a>
+                    </div>
+                </Modal>
+                <Modal visible={this.state.fadeInLeft} width="600px" height="300px" effect="fadeInLeft" onClickAway={() => this.close('fadeInLeft')}>
+                    <div className="Modal">
+                        <h1 className="Modal__title">FadeInLeft</h1>
+                        <a href="javascript:void(0);" onClick={() => this.close('fadeInLeft')}>Close</a>
+                    </div>
+                </Modal>
+                <Modal visible={this.state.fadeInRight} width="80%" height="40%" effect="fadeInRight" onClickAway={() => this.close('fadeInRight')}>
+                    <div className="Modal">
+                        <h1 className="Modal__title">FadeInRight</h1>
+                        <a href="javascript:void(0);" onClick={() => this.close('fadeInRight')}>Close</a>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
