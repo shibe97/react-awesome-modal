099a03c41d7f020ae7b602d7e3eaa405ede7c43a
kazshiba <kazshiba@yahoo-corp.jp>
2016-12-16 09:52:17 +0900

    [update] change mask position

--- src/index.js
+++ src/index.js
@@ -67,11 +67,11 @@ export default class Modal extends Component {
     render() {
         return (
             <div>
-                <div onClick={this.props.onClickAway ? this.props.onClickAway : false} style={this.state.visible ? this.state.style.mask : this.state.style.maskHidden} />
                 <div style={this.state.visible ? this.state.style.container : this.state.style.containerHidden}>
                     <div style={this.state.visible ? this.state.style.panel : this.state.style.panelHidden}>
                         {this.props.children}
                     </div>
+                    <div style={this.state.visible ? this.state.style.mask : this.state.style.maskHidden} onClick={this.props.onClickAway ? this.props.onClickAway : false} />
                 </div>
             </div>
         );
