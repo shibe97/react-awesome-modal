6876260fb08abf6b050de4dce7f8ef767c62be90
vishakc <50020557+vishakc@users.noreply.github.com>
2020-06-29 22:48:16 +0530

    Update fadeInUp.js

--- src/effect/fadeInUp.js
+++ src/effect/fadeInUp.js
@@ -12,11 +12,10 @@ export default {
         'display': 'none'
     },
     'container': {
-        'position': 'fixed',
+        'position': 'absolute',
         'top': 0,
         'left': 0,
         'width': '100%',
-        'height': '100%',
         'display': 'flex',
         'justifyContent': 'center',
         'alignItems': 'center',
